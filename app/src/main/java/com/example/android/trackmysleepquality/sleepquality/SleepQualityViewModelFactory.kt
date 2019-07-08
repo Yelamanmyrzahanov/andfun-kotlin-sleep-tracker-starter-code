4730368014b471cb5405ee68e680becab4bc2417
Yelaman Myrzakhanov <yelamanmyrzahanov@gmail.com>
2019-07-09 05:47:36 +0600

    Room completed with Dao And Kotlin Coroutines

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModelFactory.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModelFactory.kt
@@ -19,18 +19,20 @@ package com.example.android.trackmysleepquality.sleepquality
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.PrimaryKey
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.sleeptracker.SleepTrackerViewModel
 import java.lang.IllegalArgumentException
 
 class SleepQualityViewModelFactory (
-        private val dataSource: SleepDatabaseDao,
-        private val application: Application) : ViewModelProvider.Factory{
+        private val sleepNightKey:  Long,
+        private val dataSource: SleepDatabaseDao) : ViewModelProvider.Factory{
 
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(SleepTrackerViewModel::class.java)){
-            return SleepTrackerViewModel(dataSource,application) as T
+        if (modelClass.isAssignableFrom(SleepQualityViewModel::class.java)){
+            return SleepQualityViewModel(sleepNightKey,dataSource) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
